0a21f1802aa2a657ccf1f1283c5c2d19d2c32b32
sammybh6 <samyakbhargava9@gmail.com>
2023-04-09 19:45:53 +0530

    ui

--- client/src/components/BuyButton.jsx
+++ client/src/components/BuyButton.jsx
@@ -1,30 +1,53 @@
 import React from 'react'
-import { useBuyNow, useContract, Web3Button, useOwnedNFTs } from "@thirdweb-dev/react";
+import { useBuyNow, useContract, Web3Button, useOwnedNFTs, useAddress } from "@thirdweb-dev/react";
 import { ListingType } from "@thirdweb-dev/sdk";
 import { BigNumber } from 'ethers';
+import { getUserNFTs, registerUserNFTs } from '../../firebase/clientApp.mjs';
 
 const contractAddress = "0x2ED184a348935C941F821B2ac142db9C9c33EBe0";
 export default function BuyButton(props) {
   const { contract } = useContract(contractAddress, "marketplace");
   const { mutateAsync: buyNow, isLoading, error } = useBuyNow(contract);
   let id=(props.id).toString();
-  console.log(id);
-  // let st = "";
-  // id1 = BigNumber.from("1");
-  // let amt=props.amt;
-  // amt = BigNumber.from(amt);
+  const [nfts, setNfts] = React.useState([]);
+  
+
+  const userAddress = useAddress();
+    
+
   return (
+    <div>
+
     <Web3Button
       contractAddress={contractAddress}
-      action={() =>
+      action={() => {
         buyNow({
           id: `${id}`, // ID of the listing to buy
           type: ListingType.Direct, // Direct (0) or Auction (1)
           buyAmount: "1" // Amount to buy
-        })
+        });}
       }
+      
+      onSuccess={() => {
+        getUserNFTs(userAddress).then((data) => {
+            const tempData = data.nfts;
+            tempData.push(id);
+            setNfts(tempData);
+            registerUserNFTs(userAddress, tempData);
+        });
+      }}
+        onError={() => {
+            getUserNFTs(userAddress).then((data) => {
+                const tempData = nfts;
+                tempData.pop();
+                setNfts(tempData);
+                registerUserNFTs(userAddress, nfts);
+            });
+        }}
     >
+
       Buy Now
     </Web3Button>
+    </div>
   )
 }
